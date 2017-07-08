0a480ef736a909c0dde427e01a2295dda0a7d823
michaelcooper1130 <michaelcooper1130@gmail.com>
2017-07-08 00:39:03 -0700

    Added Gobs

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
     window.totalSouls = localStorage.getItem('totalSouls') || "0";
     window.souls = localStorage.getItem('souls') || "0";
     window.imps = localStorage.getItem('imps') || "0";
+    window.gobs = localStorage.getItem('gobs') || "0";
 
     this.state={
       souls: parseInt(window.souls, 10),
       imps: parseInt(window.imps, 10),
       impCost: 10,
+      gobs: parseInt(window.gobs, 10),
+      gobCost: 100,
       totalSouls: parseInt(window.totalSouls, 10),
     }
   }
@@ -43,20 +46,39 @@ class App extends Component {
     }
   }
 
+  gobIncrement = () => {
+    this.setState({
+      souls: this.state.souls + this.state.gobs,
+      gobCost: this.state.gobs * 2,
+      totalSouls: this.state.totalSouls + this.state.gobs
+    })
+  }
+
+  buyGob = () => {
+    if (this.state.souls >= this.state.gobCost) {
+      this.setState({
+        souls: this.state.souls - this.state.gobCost,
+        gobs: this.state.gobs + 1
+      });
+    }
+  }
   saveGame = () => {
     localStorage.setItem('souls', this.state.souls);
     localStorage.setItem('imps', this.state.imps);
+    localStorage.setItem('gobs', this.state.gobs);
     localStorage.setItem('totalSouls', this.state.totalSouls);
   }
 
   resetGame = () => {
     localStorage.setItem('souls', 0);
     localStorage.setItem('imps', 0);
+    localStorage.setItem('gobs', 0);
     localStorage.setItem('totalSouls', 0);
     
     this.setState({
       souls: 0,
       imps: 0,
+      gobs: 0,
       totalSouls: 0
     })
   }
@@ -86,6 +108,11 @@ class App extends Component {
           Imps: {this.state.imps}<br />
           Cost: {this.state.impCost}<br />
           <button onClick={this.buyImp}>Buy Imp</button>
+        </div>
+        <div className="App-purchases">
+          Goblins: {this.state.gobs}<br />
+          Cost: {this.state.gobCost}<br />
+          <button onClick={this.buyGob}>Buy Goblin</button>
         </div>
         <div>
           <button onClick={this.saveGame}>Save Game</button>
